ac314a1fe1b222b0e0acb35882a96b6f3c9a7b83
Jake <jake_finnegan@yahoo.com>
2017-09-02 19:25:29 -0400

    Included tablet site from internet download

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: '/../../index.html',
+  // styleUrls: ['./app.component.css']
+  // styleUrls: ['/../../css/skel-noscript.css', '/../../css/style-1000px.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My First Angular App';
 }
